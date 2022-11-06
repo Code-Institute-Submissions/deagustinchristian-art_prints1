a7237e088da2dab6c7e74aad5a48f4126afb2e1a
deagustinchristian <deagustinchristian@gmail.com>
2022-11-06 23:42:56 +0000

    Deployment part 1

--- art_prints/settings.py
+++ art_prints/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
 
 
 # Password validation
